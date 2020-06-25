00f84d9eab73ffb63674b664b6d7709968753b38
unknown <kylespinaio@hotmail.com>
2020-06-24 20:37:20 -0400

    declared playerone and playertwo variables in game logic.

--- RPSLS.js
+++ RPSLS.js
@@ -8,13 +8,23 @@ class Game{
         // this.playerTwo = new Player(name);
         this.playerTwo = null;
         // this.botPlayer = new ComputerPlayer()
-        this.choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+        this.choices = ["rock", "paper", "scissors", "lizard", "spock"];
     
 
     }
 
     runGame(){      // main method
-        this.displayRules()
+        this.displayRules();
+        this.numberOfPlayers();
+        while(true){
+            let playerOneChoice = this.playerOne.chooseGesture();
+            let playerTwoChoice = this.playerTwo.chooseGesture();
+            this.logic(playerOneChoice, playerTwoChoice);
+        }
+      
+
+
+
         // determine game mode (instantiate playerTwo as corect player type)
 
 
@@ -28,75 +38,125 @@ class Game{
 
 
     displayRules(){
-        console.log("Welcome to Rock, Paper, Scissors, Lizard, Spock.")
-        console.log("You and your oppentent will make a selection from the avaialable options.")
-        console.log("The player that wins best of 3 rounds, Wins the Game!")
+        console.log("Welcome to Rock, Paper, Scissors, Lizard, Spock.");
+        console.log("You and your oppentent will make a selection from the avaialable options.");
+        console.log("The player that wins best of 3 rounds, Wins the Game!");
     }
 
-    Logic(){  
-    if (playerChoice == "rock" & botPlayer == "rock"){
-        console.log("It's a Tie!");
+
+
+
+
+    Logic(playerOneChoice, playerTwoChoice){
+        // let playerChoice = this.playerOne.chooseGesture()
+        // let botPlayer = this.playerTwo.chooseGesture()
+
+    if (playerOneChoice == "rock" & playerTwoChoice == "rock"){
+        alert("It's a Tie!");
     }
     else if (playerChoice == "rock" & botPlayer == "paper" || botPlayer == "spock"){
-        console.log("Sorry, you lost!");
+        alert("Sorry, you lost!");
         this.botPlayer.score++;
     }
     else if (playerChoice == "rock" & botPlayer == "scissors" || botPlayer == "lizard"){
-        console.log("Yay, You won!");
+        alert("Yay, You won!");
         this.playerOne.score++;
     }
     else if (playerChoice == "paper" & botPlayer == "rock" || botPlayer == "spock"){
-        console.log("Yay, You won!");
+        alert("Yay, You won!");
         this.playerOne.score++;
     }
     else if (playerChoice == "paper" & botPlayer == "paper"){
-        console.log("It's a Tie!");
+        alert("It's a Tie!");
     }
     else if (playerChoice== "paper" & botPlayer == "scissors" || botPlayer == "lizard"){
-        console.log("Sorry, you lost!");
+        alert("Sorry, you lost!");
         this.botPlayer.score++;
     }
     else if (playerChoice == "scissors"& botPlayer == "rock" || botPlayer == "spock"){
-        console.log("Sorry, you lost!");
+        alert("Sorry, you lost!");
         this.botPlayer.score++;
     }
     else if (playerChoice == "scissors" & botPlayer == "paper" || botPlayer == "lizard"){
-        console.log("Yay, you won!");
+        alert("Yay, you won!");
         this.playerOne.score++;
     }
     else if (playerChoice == "scissors" & botPlayer == "scissors"){
-        console.log("It's a Tie!");
+        alert("It's a Tie!");
     }
     else if (playerChoice == "lizard" & botPlayer == "paper" || botPlayer == "spock"){
-        console.log("Yay, you won!");
+        alert("Yay, you won!");
         this.playerOne.score++;
     }
     else if (playerChoice == "lizard"& botPlayer == "rock" || botPlayer == "scissors"){
-        console.log("Sorry, you lost!");
+        alert("Sorry, you lost!");
         this.botPlayer.score++;
     }
     else if (playerChoice == "lizard" & botPlayer == "lizard"){
-        console.log("It's a Tie!");
+        alert("It's a Tie!");
     }
     else if (playerChoice == "spock" & botPlayer == "rock" || botPlayer == "scissors"){
-            console.log("Yay, you won!");
-            this.playerOne.score++;
+        alert("Yay, you won!");
+        this.playerOne.score++;
     }
     else if (playerChoice == "spock"& botPlayer == "paper" || botPlayer == "lizard"){
-            console.log("Sorry, you lost!");
-            this.botPlayer.score++;
+        alert("Sorry, you lost!");
+        this.botPlayer.score++;
     }
     else if (playerChoice == "spock" & botPlayer == "spock"){
-                console.log("It's a Tie!");
+        alert("It's a Tie!");
     }
      
     else{
-        console.log("Please select again")
+        console.log("Please select again");
     }
 
-    return gameResult
+    return gameResult;
     }
+
+
+    scoreBoard(){
+        //for (i = 0; i  )
+    }
+
+
+
+    numberOfPlayers(){
+        let chooseNumberOfPlayers = prompt("Press 1 to play against another player. Press 2 to play against the computer: ")
+        switch (chooseNumberOfPlayers){
+            case "1":
+                let userInput = prompt("what is the name of player two? ");
+                this.playerTwo = new Player(userInput);
+                break;
+            case "2":
+                this.playerTwo = new ComputerPlayer();
+                break;
+        }
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
 
 
 
@@ -105,22 +165,22 @@ class Player{
 
         this.score = 0;
         this.name = name;  
-
+        
     }
 
     chooseGesture() {
-        // logic to allow human to choose gesture
+        let gesture = prompt("Please make a selection. Please choose: rock, paper, scissors, lizard, or spock? ");
+        let playerChoice = gesture.toLocaleLowerCase()
+        return playerChoice
+
+        
     }
-
-
-
-
 }
 
 class ComputerPlayer extends Player{
-    constructor(name){
-        super();
-        this.name = "CPU"
+    constructor(){
+        super("CPU");
+        
     }
 
     chooseGesture(choices){
@@ -129,15 +189,17 @@ class ComputerPlayer extends Player{
     }
 }
 
-let gameCounter = 0
+// let gameCounter = 0
+function BeginGame(){
+    let game = new Game()
+    game.runGame();
+}
 
-// let game = new Game()
-// game.runGame()
 
-let testComputerPlayer = new ComputerPlayer("CPU");
-let result = testComputerPlayer.chooseGesture();
+// let testComputerPlayer = new ComputerPlayer("CPU");
+// let result = testComputerPlayer.chooseGesture();
 
-// let computerTest = new Player("carl");
-// let testResult = computerTest.computerGenerateRandomChoice()
-// console.log(testResult)
+// // let computerTest = new Player("carl");
+// // let testResult = computerTest.computerGenerateRandomChoice()
+// // console.log(testResult)
 
